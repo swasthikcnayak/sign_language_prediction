809b58ee9b6b0b575d9520ee3f30e1e88debd6c7
swasthik <swasthik0304@gmail.com>
2020-11-09 01:17:04 +0530

    added flask implementation

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-from PIL import Image,ImageOps
-from model.model import *
-from tensorflow.keras.models import load_model
+from flask import Flask, request, redirect, url_for, render_template
+from tensorflow_core.python.keras.saving.save import load_model
+from werkzeug import secure_filename
+from PIL import ImageOps, Image
 import os
-from helper.helper import *
+import numpy as np
 
-mean = 0
-std_deviation = 0
+app = Flask(__name__)
 
-if os.path.isfile("savedModel"):
-    model = load_model("savedModel")
-else:
-    train, test = load_data()
-    X_train, Y_train = split_and_transform(train)
-    X_test, Y_test = split_and_transform(test)
-    model = create_model(X_train, Y_train, X_test, Y_test)
-    model.save('savedModel', save_format="h5")
+model = load_model('model.h5')
 
-filename = "test-images/test3.jpg"
-img = Image.open(filename)
-img = ImageOps.grayscale(img)
-img = img.resize((28,28))
-category = model.predict_classes(img)
-print(category)
-for i in category:
-    print(i)
+
+@app.route('/', methods=['GET', 'POST'])
+def index_page():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(filename)
+        return redirect(url_for('prediction', filename=filename))
+    return render_template('index.html')
+
+
+@app.route('/prediction/<filename>')
+def prediction(filename):
+    img = Image.open(filename)
+    img = ImageOps.grayscale(img)
+    img = img.resize((28, 28))
+    img = np.array(img)
+    img = np.reshape(img, newshape=(1, 28, 28, 1))
+    category = model.predict_classes(img)
+    charter = 'a'
+    if category >= 9:
+        category = category-1
+    charter = chr(category + 64)
+    return render_template('predict.html', category=category,
+                           char=charter)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
